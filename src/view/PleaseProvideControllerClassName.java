82de06d8928c4f071c1f6188f02c23c35d7d26d1
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-03-29 20:20:12 -0300

    Foi criado uma classe para manipulacao dos dados.

---
+++ src/view/PleaseProvideControllerClassName.java
@@ -0,0 +1,47 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField txfId;
+
+    @FXML
+    private TextField txfNome;
+
+    @FXML
+    private TextField txfTelefone;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Button btnLimpar;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    void excluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvar(ActionEvent event) {
+
+    }
+
+}
